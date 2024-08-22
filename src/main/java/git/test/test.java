1f8d2ead4ce2e4e091c6d6ec59078ca157a66228
Rushikesh Mehta <rushikesh.mehta@agiliad.local>
2024-08-22 14:25:03 +0530

    fristCommit

---
+++ src/main/java/git/test/test.java
@@ -0,0 +1,21 @@
+package git.test;
+
+public class test {
+	public static void main(String args[]) {
+     
+		
+		
+		
+		
+		int number = 100;
+		String name = "Rushi";
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
